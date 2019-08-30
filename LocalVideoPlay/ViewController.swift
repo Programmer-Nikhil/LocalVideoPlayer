fdd04515bb408027bb61c0a12a656bda61348809
Nikhil Devgire <n.Devgire@gmail.com>
2019-08-30 09:40:39 +0530

    Initial Commit

---
+++ LocalVideoPlay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalVideoPlay
+//
+//  Created by Nikhil Devgire on 30/08/19.
+//  Copyright © 2019 Nikhil Devgire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
